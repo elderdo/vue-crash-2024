eb42f1a062d78dbe99ce469b800e30d9ca25f49f
elderdo <elderdo@yahoo.com>
2025-01-22 14:20:37 -0600

    added button and function to toggle status

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,49 @@
 <script>
 
-  export default {
-    name: 'App',
-    data() {
-      return {
-        name: 'John Doe',
-        status: 'pending',
-        tasks: ['Task One', 'Task Two', 'Task Three'],
-        link: 'https://www.google.com'
-      }
-    },
-    created() {
-      fetch('https://jobs.github.com/positions.json')
-        .then(response => response.json())
-        .then(data => {
-          this.jobs = data
-        })
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://www.google.com'
     }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    }
+  },
+  created() {
+    fetch('https://jobs.github.com/positions.json')
+      .then(response => response.json())
+      .then(data => {
+        this.jobs = data
+      })
   }
+}
 </script>
 
 <template>
   <h1>{{ name }}</h1>
+  <br>
   <p v-if="status === 'active'">Status: Active</p>
   <p v-else-if="status === 'pending'">Status: Pending</p>
   <p v-else>Status: Inactive</p>
-
+  <br>
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li> 
-    <!-- <a v-bind:href="link">Google</a> -->
-     <a :href="link">Google</a>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
+  <br>
+  <button v-on:click="toggleStatus">Toggle Status</button>
 </template>
-
